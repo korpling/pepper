dfef6d4f786b81853812c6ad7800168037dc8354
FlorianZipser <f.zipser@gmx.de>
2013-08-29 12:49:13 +0000

    updated salt dependency to 1.1.8-SNAPSHOT
    git-svn-id: https://korpling.german.hu-berlin.de/svn/saltnpepper/pepper/trunk@8453 47085195-c50e-4317-8f43-7cb808db0f00

---
+++ pepper-core/src/main/java/de/hu_berlin/german/korpling/saltnpepper/pepper/core/PepperJob.java
@@ -0,0 +1,5 @@
+package de.hu_berlin.german.korpling.saltnpepper.pepper.core;
+
+public class PepperJob {
+
+}
---
+++ pepper-core/src/main/java/de/hu_berlin/german/korpling/saltnpepper/pepper/coreImpl/PepperJobImpl.java
@@ -0,0 +1,5 @@
+package de.hu_berlin.german.korpling.saltnpepper.pepper.coreImpl;
+
+public class PepperJobImpl {
+
+}
