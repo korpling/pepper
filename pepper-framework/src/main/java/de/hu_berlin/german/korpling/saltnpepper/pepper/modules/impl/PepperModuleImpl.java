korpling/pepper

commit f7683c4542ad433c2cf0332f64447387f7a26e9f

fixed a bug in PepperModuleImpl, which causes that for each corpus the method start(SElementId) is called even for importers, which is a problem for the importers.

pepper-framework/src/main/java/de/hu_berlin/german/korpling/saltnpepper/pepper/modules/impl/PepperModuleImpl.java
@@ -657,8 +657,20 @@ public class PepperModuleImpl implements PepperModule, UncaughtExceptionHandler 
 		List<SCorpusGraph> corpGraphs = Collections.synchronizedList(this.getSaltProject().getSCorpusGraphs());
 		for (SCorpusGraph sCorpusGraph : corpGraphs) {
 			if (sCorpusGraph != null) {
-				for (SCorpus sCorpus : sCorpusGraph.getSCorpora()) {
-					corporaToEnd.add(sCorpus);
+				if (MODULE_TYPE.IMPORTER.equals(getModuleType())){
+					//in case of module is an importer, only import corpora from set corpus graph
+					if (sCorpusGraph.equals(((PepperImporter)this).getSCorpusGraph())){
+						for (SCorpus sCorpus : sCorpusGraph.getSCorpora()) {
+							corporaToEnd.add(sCorpus);
+						}
+					}
+					
+				}else{
+					//if module is not an importer,  process all corpora 
+				
+					for (SCorpus sCorpus : sCorpusGraph.getSCorpora()) {
+						corporaToEnd.add(sCorpus);
+					}
 				}
 			}
 		}
@@ -669,8 +681,9 @@ public class PepperModuleImpl implements PepperModule, UncaughtExceptionHandler 
 			PepperMapperController controller = new PepperMapperControllerImpl(mapperThreadGroup, this.getName() + "_mapper(" + sElementId.getSId() + ")");
 
 			String id = sElementId.getSId();
-			if (sElementId.getSIdentifiableElement() instanceof SDocument)
+			if (sElementId.getSIdentifiableElement() instanceof SDocument){
 				id = SaltFactory.eINSTANCE.getGlobalId(sElementId);
+			}
 			this.getMapperControllers().put(id, controller);
 			controller.setUncaughtExceptionHandler(this);
 			controller.setPepperModule(this);

commit 593e13b617f758b048e507c7902937a2261f2164

fixed a bug occuring in SamplImporter in pepper-sampleModules, which was caused with  commit f7683c45

pepper-framework/src/main/java/de/hu_berlin/german/korpling/saltnpepper/pepper/modules/impl/PepperModuleImpl.java
@@ -664,7 +664,6 @@ public class PepperModuleImpl implements PepperModule, UncaughtExceptionHandler 
 							corporaToEnd.add(sCorpus);
 						}
 					}
-					
 				}else{
 					//if module is not an importer,  process all corpora 
 				

pepper-sampleModules/src/main/java/de/hu_berlin/german/korpling/saltnpepper/pepperModules/sampleModules/SampleImporter.java
@@ -153,6 +153,7 @@ public class SampleImporter extends PepperImporterImpl implements PepperImporter
 		 */
 		//super.importCorpusStructure(sCorpusGraph);
 		
+		this.setSCorpusGraph(sCorpusGraph);
 		// creates the super-corpus c1, in Salt you can create corpora via a URI
 		SCorpus c1= sCorpusGraph.createSCorpus(URI.createURI("salt:/c1")).get(0);
 		//creates the sub-corpora c2 and c3, in Salt you can also create corpora adding a corpus to a parent