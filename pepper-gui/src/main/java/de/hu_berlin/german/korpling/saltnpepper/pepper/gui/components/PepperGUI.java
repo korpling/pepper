f36a708be77f032e8ac67b5b40c0f976164e30f5
martink <martin.klotz@hu-berlin.de>
2015-04-17 13:01:32 +0200

    began work on declarative layout

--- pepper-gui/src/main/java/de/hu_berlin/german/korpling/saltnpepper/pepper/gui/components/PepperGUI.java
+++ pepper-gui/src/main/java/de/hu_berlin/german/korpling/saltnpepper/pepper/gui/components/PepperGUI.java
@@ -8,15 +8,22 @@ import de.hu_berlin.german.korpling.saltnpepper.pepper.gui.controller.PepperGUIC
 public class PepperGUI extends VerticalLayout {
 	protected static final String GUI_WIDTH = "1024px";
 	protected static final String GUI_HEIGHT = "768px";	
-	
-	/*MAYBE?*/
-	private HorizontalLayout footer;
-	
+		
 	public PepperGUI(PepperGUIController GUIcontroller){		
 		setWidth(GUI_WIDTH);
 		setHeight(GUI_HEIGHT);		
 //		addComponent(new PepperGUIHeader(GUIcontroller));				
-		addComponent(new PepperGUIBody(GUIcontroller));
+//		addComponent(new PepperGUIBody(GUIcontroller));
 		
+		HorizontalLayout header = new HorizontalLayout();
+		header.setHeight(PepperGUIHeader.GUI_HEADER_HEIGHT);
+		header.setWidth(GUI_WIDTH);
+		header.setCaption("HEADER");
+		HorizontalLayout body = new HorizontalLayout();
+		body.setHeight(PepperGUIBody.GUI_BODY_HEIGHT);
+		body.setWidth(GUI_WIDTH);
+		body.setCaption("BODY");
+		addComponent(header);
+		addComponent(body);
 	}
 }
