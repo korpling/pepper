54ea845c042bb162f3d53f96b297f47662bdd883
f.zipser@gmx.de <f.zipser@gmx.de>
2015-07-30 14:39:08 +0200

    adapted changelog

--- pepper-lib/src/main/java/de/hu_berlin/german/korpling/saltnpepper/pepper/cli/PepperStarter.java
+++ pepper-lib/src/main/java/de/hu_berlin/german/korpling/saltnpepper/pepper/cli/PepperStarter.java
@@ -83,7 +83,7 @@ public class PepperStarter {
 	 * in and outside of OSGi. This could be removed, when there is a better way
 	 * to detect the current Pepper version automatically.
 	 */
-	public static final String PEPPER_VERSION = "2.1.1.SNAPSHOT";
+	public static final String PEPPER_VERSION = "2.1.2";
 
 	/**
 	 * A logger for logging messages.
