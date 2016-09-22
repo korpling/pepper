e5db8e86d0382093c462d75eb3101c83699e5f32
Martin Klotz <MartinKl@hu-berlin.de>
2016-09-22 18:57:32 +0200

    now it works, the locator stuff (hk2...) NEEDS to be a bundle, does not work as dependency. Be aware that javax.inject is not osgi per default, but there is an osgi version

--- pepper-gui/src/main/java/org/corpus_tools/pepper/gui/client/ServiceConnector.java
+++ pepper-gui/src/main/java/org/corpus_tools/pepper/gui/client/ServiceConnector.java
@@ -14,43 +14,51 @@ import javax.ws.rs.core.Response;
 
 import org.corpus_tools.pepper.common.PepperModuleDesc;
 import org.corpus_tools.pepper.service.adapters.PepperModuleCollectionMarshallable;
+import org.corpus_tools.pepper.service.adapters.PepperModuleDescMarshallable;
 import org.corpus_tools.pepper.service.util.PepperSerializer;
 import org.corpus_tools.pepper.service.util.PepperServiceURLDictionary;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class ServiceConnector implements PepperServiceURLDictionary{
 	private String serviceUrl;
 	public Client client = null;
-	public WebTarget baseTarget = null;
+	public WebTarget baseTarget = null;	
 	
-	private PepperSerializer serializer = null; 
+	private final PepperSerializer serializer;
+	
+	private static final Logger logger = LoggerFactory.getLogger(ServiceConnector.class);
+	private static final String ERR_404 = "Requested resource not available";
 	
 	public ServiceConnector(String serviceURL){
 		this.serviceUrl = serviceURL;
 		this.client = ClientBuilder.newClient();
-		try{
-			this.baseTarget = client.target(serviceUrl);
-		} catch (Exception e){
-			e.printStackTrace();
-		}
-		
+		this.baseTarget = client.target(serviceUrl);		
 		serializer = PepperSerializer.getInstance("application/xml");
+		
+		logger.info("ServiceConnector initialized with base target" + baseTarget + " and Serializer " + serializer);
 	}
 
 	public Collection<PepperModuleDesc> getAllModules() {
-		if (baseTarget != null){
-			WebTarget target = baseTarget.path(PATH_ALL_MODULES);
-			Builder rb = target.request();
-			Invocation getInvocation = rb.buildGet();
-			Future<Response> futResponse = getInvocation.submit();
-			Response response = null;
-			try {
-				response = futResponse.get();
-			} catch (InterruptedException | ExecutionException e) {
-				e.printStackTrace();
-			}
-			System.out.println("R E S P O N S E:::" + response);
-			serializer.getUnmarshaller(PepperModuleCollectionMarshallable.class);
+		ArrayList<PepperModuleDesc> moduleList = new ArrayList<PepperModuleDesc>(); 
+		WebTarget target = baseTarget!=null? baseTarget.path(PATH_ALL_MODULES) : client.target("http://localhost:8080/pepper-rest/resource/modules");
+		Builder rb = target.request();
+		Invocation getInvocation = rb.buildGet();
+		Future<Response> futResponse = getInvocation.submit();
+		Response response = null;
+		try {
+			response = futResponse.get();
+		} catch (InterruptedException | ExecutionException e) {
+			e.printStackTrace();
 		}
-		return new ArrayList<PepperModuleDesc>();
+		logger.info("Received response: " + response);
+		if (response.getStatus() == 404){
+			logger.error(ERR_404);
+		} else {
+			for (PepperModuleDescMarshallable pmdm : ((PepperModuleCollectionMarshallable)serializer.unmarshal(null, PepperModuleCollectionMarshallable.class)).getModuleList()){
+				moduleList.add(pmdm.getPepperObject());
+			}
+		}
+		return moduleList;
 	}
 }
